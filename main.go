5deaa8fcfff2a6007e713554c4c48030d1e2d4f6
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2023-02-05 19:11:14 +0900

    mux関数追加

--- main.go
+++ main.go
@@ -5,19 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/Satoshi-Tb/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -28,31 +20,10 @@ func run(ctx context.Context) error {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown %+v", err)
-	}
-
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
-	return eg.Wait()
+	return s.Run(ctx)
 }
 
 func main() {
