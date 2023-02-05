ba934f25260d386140202b4c6ff0206f0f076b06
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2023-02-05 17:13:15 +0900

    シグナルハンドリグ処理追加

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Satoshi-Tb/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	cfg, err := config.New()
 	if err != nil {
 		return err
